8c6e0b7beb6093ec187bc5a4b9271a743f99afda
구제서 <jsgu@koreanair.com>
2023-03-28 10:21:40 +0900

    eks commit

--- 6-eks.tf
+++ 6-eks.tf
@@ -16,3 +16,8 @@ resource "aws_iam_role" "eks-cluster" {
 }
 POLICY
 }
+
+resource "aws_iam_role_policy_attachment" "amazon-eks-cluster-policy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+  role       = aws_iam_role.eks-cluster.name
+}
