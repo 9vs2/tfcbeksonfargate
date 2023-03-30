600166d7e83db28b825ca84555d660fbb4eef2c6
구제서 <jsgu@koreanair.com>
2023-03-30 17:30:50 +0900

    kube commit

--- 7-kube-system-profile.tf
+++ 7-kube-system-profile.tf
@@ -17,3 +17,20 @@ resource "aws_iam_role_policy_attachment" "eks-fargate-profile" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSFargatePodExecutionRolePolicy"
   role       = aws_iam_role.eks-fargate-profile.name
 }
+
+resource "aws_eks_fargate_profile" "kube-system" {
+  cluster_name           = aws_eks_cluster.cluster.name
+  fargate_profile_name   = "kube-system"
+  pod_execution_role_arn = aws_iam_role.eks-fargate-profile.arn
+
+  # These subnets must have the following resource tag: 
+  # kubernetes.io/cluster/<CLUSTER_NAME>.
+  subnet_ids = [
+    aws_subnet.private-ap-northeast-3a.id,
+    aws_subnet.private-ap-northeast-3b.id
+  ]
+
+  selector {
+    namespace = "kube-system"
+  }
+}
