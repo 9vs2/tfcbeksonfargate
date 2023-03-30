df26340b82bcb44c7ced82b4f3900ca81e72195c
구제서 <jsgu@koreanair.com>
2023-03-30 16:21:13 +0900

    kube commit

---
+++ 7-kube-system-profile.tf
@@ -0,0 +1,14 @@
+resource "aws_iam_role" "eks-fargate-profile" {
+  name = "eks-fargate-profile"
+
+  assume_role_policy = jsonencode({
+    Statement = [{
+      Action = "sts:AssumeRole"
+      Effect = "Allow"
+      Principal = {
+        Service = "eks-fargate-pods.amazonaws.com"
+      }
+    }]
+    Version = "2012-10-17"
+  })
+}
