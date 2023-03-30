95f7a16daac063cdc0c3d16b9bfe7e7e5aa8ae5b
구제서 <jsgu@koreanair.com>
2023-03-30 13:30:31 +0900

    eks commit

--- 6-eks.tf
+++ 6-eks.tf
@@ -21,3 +21,27 @@ resource "aws_iam_role_policy_attachment" "amazon-eks-cluster-policy" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
   role       = aws_iam_role.eks-cluster.name
 }
+
+
+
+resource "aws_eks_cluster" "cluster" {
+  name     = var.cluster_name
+  version  = var.cluster_version
+  role_arn = aws_iam_role.eks-cluster.arn
+
+  vpc_config {
+
+    endpoint_private_access = false
+    endpoint_public_access  = true
+    public_access_cidrs     = ["0.0.0.0/0"]
+
+    subnet_ids = [
+      aws_subnet.private-ap-northeast-3a.id,
+      aws_subnet.private-ap-northeast-3b.id,
+      aws_subnet.public-ap-northeast-3a.id,
+      aws_subnet.public-ap-northeast-3b.id
+    ]
+  }
+
+  depends_on = [aws_iam_role_policy_attachment.amazon-eks-cluster-policy]
+}
