ac53787fb47aeedfcf61c04086ce876e4067f048
구제서 <jsgu@koreanair.com>
2023-03-31 10:49:11 +0900

    eks commit

--- 6-eks.tf
+++ 6-eks.tf
@@ -1,3 +1,4 @@
+/*
 resource "aws_iam_role" "eks-cluster" {
   name = "eks-cluster-${var.cluster_name}"
 
@@ -58,5 +59,66 @@ module "eks_auth" {
 
   map_accounts = [
     "789535401130"
+  ]
+}
+*/
+
+
+module "eks" {
+  source  = "terraform-aws-modules/eks/aws"
+  version = "~> 19.0"
+
+  cluster_name    = var.cluster_name
+  cluster_version = var.cluster_version
+
+  cluster_endpoint_public_access  = true
+
+  cluster_addons = {
+    coredns = {
+      most_recent = true
+    }
+    kube-proxy = {
+      most_recent = true
+    }
+    vpc-cni = {
+      most_recent = true
+    }
+  }
+
+  vpc_id                   = aws_vpc.main.id
+  subnet_ids               = [
+      aws_subnet.private-ap-northeast-3a.id,
+      aws_subnet.private-ap-northeast-3b.id,
+      aws_subnet.public-ap-northeast-3a.id,
+      aws_subnet.public-ap-northeast-3b.id
+    ]
+  //control_plane_subnet_ids = ["subnet-xyzde987", "subnet-slkjf456", "subnet-qeiru789"]
+  iam_role_arn = aws_iam_role.eks-cluster.arn
+
+  # Fargate Profile(s)
+  fargate_profiles = {
+    default = {
+      name = "kube-system"
+      selectors = [
+        {
+          namespace = "kube-system"
+        }
+      ]
+    }
+  }
+
+  # aws-auth configmap
+  manage_aws_auth_configmap = true
+
+  aws_auth_roles = [
+    {
+      rolearn  = "arn:aws:sts::789535401130:assumed-role/CCOE/CCOE@KESBOXCOE"
+      username = "role1"
+      groups   = ["system:masters"]
+    },
+  ]
+
+  aws_auth_accounts = [
+    "789535401130",
   ]
 }
