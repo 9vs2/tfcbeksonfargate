3142a8519bedfc973d8153fd6fc6ebd650ba5f09
구제서 <jsgu@koreanair.com>
2023-04-07 13:37:20 +0900

    tfcb commit

--- 8-eks-aws-auth.tf
+++ 8-eks-aws-auth.tf
@@ -10,3 +10,17 @@ provider "kubernetes" {
   token                  = data.aws_eks_cluster_auth.cluster.token 
 }
 
+output "cluster_id" {
+  description = "The name/id of the EKS cluster."
+  value       = aws_eks_cluster.cluster.id
+}
+
+output "cluster_endpoint" {
+  description = "The endpoint for your EKS Kubernetes API."
+  value       = aws_eks_cluster.cluster.endpoint
+}
+
+output "cluster_certificate_authority_data" {
+  description = "Nested attribute containing certificate-authority-data for your cluster. This is the base64 encoded certificate data required to communicate with your cluster."
+  value       = aws_eks_cluster.cluster.certificate_authority[0].data
+}
