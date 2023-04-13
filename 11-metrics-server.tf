0133073a5ce2cd507eefe7f9573e2f2087cfdaa2
구제서 <jsgu@koreanair.com>
2023-04-13 09:11:06 +0900

    tfcb commit

---
+++ 11-metrics-server.tf
@@ -0,0 +1,29 @@
+# Datasource: 
+data "aws_eks_cluster_auth" "cluster" {
+  name = aws_eks_cluster.cluster.id
+}
+
+provider "helm" {
+  kubernetes {
+    host                   = aws_eks_cluster.cluster.endpoint
+    cluster_ca_certificate = base64decode(aws_eks_cluster.cluster.certificate_authority[0].data)
+    token                  = data.aws_eks_cluster_auth.cluster.token 
+  }
+}
+
+
+resource "helm_release" "metrics-server" {
+  name = "metrics-server"
+
+  repository = "https://kubernetes-sigs.github.io/metrics-server/"
+  chart      = "metrics-server"
+  namespace  = "kube-system"
+  version    = "3.8.2"
+
+  set {
+    name  = "metrics.enabled"
+    value = false
+  }
+
+  depends_on = [aws_eks_fargate_profile.kube-system]
+}
